b6238a2d55df76defbf9fc877398ed05632f3927
argenadenov <argen.adenov@emoldino.com>
2022-07-05 23:48:48 +0900

    test api

--- app.py
+++ app.py
@@ -27,13 +27,50 @@ def about():
 def reference():
     return render_template('reference.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/result', methods=['POST'])
 def my_form_post():
-    user_input = request.form['text']
+
+    """ #Fingerprint
+    f_extended_connectivity = request.form['fec']
+    f_atom_environments = request.form['fae']
+    f_topological_torsion = request.form['ftts']
+    f_atom_pair = request.form['fap']
+
+    #Output type
+    smiles = request.form['smiles']
+    selfies = request.form['selfies']
+
+    #Tverskiy index parameter
+    alpha = request.form['alpha']
+    beta = request.form['beta']
+
+    #Fingerprint class for molecular similarity
+    predefined_sub = request.form['predefined_sub']
+    path_feature = request.form['path_feature']
+    path_based = request.form['path_based']
+    four_atom = request.form['four_atom']
+    circular = request.form['circular']
+
+    #Fingerprint type for molecular similarity
+    atom_pair_hashed = request.form['predefined_sub']
+    atom_pair_hashed = request.form['rdk_with']
+    atom_pair_hashed = request.form['rdk_without']
+
+    #others
+    macc = request.form['macc']
+    avalon = request.form['avalon']
+    atom_hashed_pair = request.form['atom_hashed_pair'] """
+
+    input='1 80 94 114 237 241 255 294 392 411 425 695 743 747 786 875 1057 1171 1238 1365 1380 1452 1544 1750 1773 1853 1873 1970'
+
+    model_type='smiles'
+    result = model_call(input, "TT", model_type)
+
+
 
     #result = model_call(user_input)
 
-    return(render_template('predict.html', variable=user_input))
+    return(render_template('result.html', variable=result))
 
 if __name__ == "__main__":
     app.run(debug=True)
